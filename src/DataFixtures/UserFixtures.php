09bbbc74f095532ee41487ad785d6da3c5a09b58
Cynthia <cynthia.louis@phc-holding.com>
2019-07-12 15:27:49 +0200

    authentication and roles

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -36,6 +36,19 @@ class UserFixtures extends BaseFixture
             return $user;
         });
 
+        $this->createMany(3, 'admin_users', function($i) {
+            $user = new User();
+            $user->setEmail(sprintf('admin%d@example.com', $i));
+            $user->setFirstName($this->faker->firstName);
+            $user->setPassword($this->passwordEncoder->encodePassword(
+                $user,
+                'engage'
+            ));
+            $user->setRoles(['ROLE_ADMIN']);
+
+            return $user;
+        });
+
         $manager->flush();
     }
 }
